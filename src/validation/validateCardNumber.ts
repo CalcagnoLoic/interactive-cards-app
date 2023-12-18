46f4a093cab224cbddf5ee7010068f828ffa9add
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-18 17:56:49 +0100

    refactoring validate functions with switch case

--- src/validation/validateCardNumber.ts
+++ src/validation/validateCardNumber.ts
@@ -5,22 +5,28 @@ export const validateCardNumber = (element: HTMLInputElement): boolean => {
     ".error-message-card-number",
   ) as HTMLInputElement;
   const re = /[a-z]/gi;
+  const matchResult = element.value.match(re);
 
-  if (element.value === "") {
-    showingErrorMessage(spanNode, "Can't be blank", "#FF5252");
-    return false;
-  } else if (element.value.match(re)) {
-    showingErrorMessage(
-      spanNode,
-      "Wrong format, can be only numeric",
-      "#FF5252",
-    );
-    return false;
-  } else if (element.value.length < 16) {
-    showingErrorMessage(spanNode, "There aren't enough characters", "#FF5252");
-    return false;
-  } else {
-    showingErrorMessage(spanNode, "", "#FF5252");
-    return true;
+  switch (true) {
+    case element.value === "":
+      showingErrorMessage(spanNode, "Can't be blank", "#FF5252");
+      return false;
+    case matchResult !== null:
+      showingErrorMessage(
+        spanNode,
+        "Wrong format, can be only numeric",
+        "#FF5252",
+      );
+      return false;
+    case element.value.length < 16:
+      showingErrorMessage(
+        spanNode,
+        "There aren't enough characters",
+        "#FF5252",
+      );
+      return false;
+    default:
+      showingErrorMessage(spanNode, "", "#FF5252");
+      return true;
   }
 };
--- src/validation/validateExpDate.ts
+++ src/validation/validateExpDate.ts
@@ -5,22 +5,28 @@ export const validateExpDate = (element: HTMLInputElement): boolean => {
     ".error-message-expiration-date",
   ) as HTMLInputElement;
   const re = /\D/gi;
+  const matchResult = element.value.match(re);
 
-  if (element.value === "") {
-    showingErrorMessage(spanNode, "Can't be blank", "#FF5252");
-    return false;
-  } else if (element.value.match(re)) {
-    showingErrorMessage(
-      spanNode,
-      "Wrong format, can be only numeric",
-      "#FF5252",
-    );
-    return false;
-  } else if (element.value.length < 2) {
-    showingErrorMessage(spanNode, "There aren't enough characters", "#FF5252");
-    return false;
-  } else {
-    showingErrorMessage(spanNode, "", "#FF5252");
-    return true;
+  switch (true) {
+    case element.value === "":
+      showingErrorMessage(spanNode, "Can't be blank", "#FF5252");
+      return false;
+    case matchResult !== null:
+      showingErrorMessage(
+        spanNode,
+        "Wrong format, can be only numeric",
+        "#FF5252",
+      );
+      return false;
+    case element.value.length < 2:
+      showingErrorMessage(
+        spanNode,
+        "There aren't enough characters",
+        "#FF5252",
+      );
+      return false;
+    default:
+      showingErrorMessage(spanNode, "", "#FF5252");
+      return true;
   }
 };
