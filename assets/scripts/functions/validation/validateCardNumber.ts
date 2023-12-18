a0b7beb1be7162489e5e7a8e95292e808397cb0e
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-18 09:37:14 +0100

    fixing pattern of validation

--- assets/scripts/functions/validation/validateCardNumber.ts
+++ assets/scripts/functions/validation/validateCardNumber.ts
@@ -4,7 +4,7 @@ export const validateCardNumber = (element: HTMLInputElement): boolean => {
     const spanNode = document.querySelector(
         ".error-message-card-number",
     ) as HTMLInputElement;
-    const re = /^(\d{0,4})\s(\d{0,4})\s(\d{0,4})\s(\d{0,4})$/;
+    const re = /[a-z]/gi;
 
     if (element.value === "") {
         showingErrorMessage(spanNode, "Can't be blank", "#FF5252");
