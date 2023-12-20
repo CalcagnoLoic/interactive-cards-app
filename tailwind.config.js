bb2a7aeff39e2402c3bffd2a3ada680410f12faa
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-20 15:40:09 +0100

    refactoring link css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const COLORS = {
-  electric_violet: "#6448FE",
-  purple: "#600594",
-};
-
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["*.html"],
   theme: {
     extend: {
       colors: {
-        linear_gradient: `${COLORS.electric_violet} to ${COLORS.purple}`,
         sunset_orange: "#FF5252",
         white: "#FFFFFF",
         mischka: "#DEDDDF",
