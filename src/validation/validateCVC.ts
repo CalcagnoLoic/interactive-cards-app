895b1b43e5238e4ccb030a2da8585a6747f373e0
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2023-12-20 10:11:44 +0100

    refactoring the project

--- src/validation/validateCVC.ts
+++ src/validation/validateCVC.ts
@@ -1,24 +1,15 @@
-import { showingErrorMessage } from "./showingErrorMessage.js";
-
-export const validateCVC = (element: HTMLInputElement): boolean => {
-  const spanNode = document.querySelector(
-    ".error-message-code-cvc",
-  ) as HTMLInputElement;
+export const validateCVC = (cvcValue: string): [boolean, string] => {
   const re = /\D/gi;
-  const matchResult = element.value.match(re);
+  const matchResult = cvcValue.match(re);
 
   switch (true) {
-    case element.value === "":
-      showingErrorMessage(spanNode, "Can't be blank");
-      return false;
+    case cvcValue === "":
+      return [false, "Can't be blank"];
     case matchResult !== null:
-      showingErrorMessage(spanNode, "Wrong format, can be only numeric");
-      return false;
-    case element.value.length < 3:
-      showingErrorMessage(spanNode, "There aren't enough characters");
-      return false;
+      return [false, "Wrong format, can be only numeric"];
+    case cvcValue.length < 3:
+      return [false, "There aren't enough characters"];
     default:
-      showingErrorMessage(spanNode, "");
-      return true;
+      return [true, ""];
   }
 };
--- src/validation/validateCardNumber.ts
+++ src/validation/validateCardNumber.ts
@@ -1,24 +1,15 @@
-import { showingErrorMessage } from "./showingErrorMessage.js";
-
-export const validateCardNumber = (element: HTMLInputElement): boolean => {
-  const spanNode = document.querySelector(
-    ".error-message-card-number",
-  ) as HTMLInputElement;
+export const validateCardNumber = (cardNumberValue: string): [boolean, string] => {
   const re = /[a-z]/gi;
-  const matchResult = element.value.match(re);
+  const matchResult = cardNumberValue.match(re);
 
   switch (true) {
-    case element.value === "":
-      showingErrorMessage(spanNode, "Can't be blank");
-      return false;
+    case cardNumberValue === "":
+      return [false, "Can't be blank"]
     case matchResult !== null:
-      showingErrorMessage(spanNode, "Wrong format, can be only numeric");
-      return false;
-    case element.value.length < 16:
-      showingErrorMessage(spanNode, "There aren't enough characters");
-      return false;
+      return [false, "Wrong format, can be only numeric"]
+    case cardNumberValue.length < 16:
+      return [false, "There aren't enough characters"]
     default:
-      showingErrorMessage(spanNode, "");
-      return true;
+      return [true, ""]
   }
 };
